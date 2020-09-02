46536544fff4503db3a581c92dd8f31104d15677
Colin Brayton <gringolalia@gmail.com>
2020-09-02 13:37:59 -0300

    deploy

---
+++ precache-manifest.38c34c936bf17d11761454fda8a5a260.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948d1bbdcf50f34bb064",
+    "url": "/css/about.f46b00f1.css"
+  },
+  {
+    "revision": "d06e608a62099a235e8f",
+    "url": "/css/app.2f3a5b9c.css"
+  },
+  {
+    "revision": "b615cd18cf837640ac52",
+    "url": "/css/chunk-vendors.92e56d25.css"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "3fea563c5316b0039389b59f0a876105",
+    "url": "/img/hammer-sickle.3fea563c.svg"
+  },
+  {
+    "revision": "be5f74c71accb0c2de02990aa4aab159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948d1bbdcf50f34bb064",
+    "url": "/js/about.49594b22.js"
+  },
+  {
+    "revision": "d06e608a62099a235e8f",
+    "url": "/js/app.5d1bb920.js"
+  },
+  {
+    "revision": "b615cd18cf837640ac52",
+    "url": "/js/chunk-vendors.46670a1f.js"
+  },
+  {
+    "revision": "5db442a300b2657dee51e4df9eb8548c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
